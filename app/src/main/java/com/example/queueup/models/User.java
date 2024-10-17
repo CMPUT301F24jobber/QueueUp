85dd0b769c60b14d8c4e2a74a913dde93f99ac02
Richard Wu <imrichardwu@Richards-MacBook-Air.local>
2024-10-16 18:32:18 -0600

    user profile setup

--- app/src/main/java/com/example/queueup/models/User.java
+++ app/src/main/java/com/example/queueup/models/User.java
@@ -1,4 +1,101 @@
 package com.example.queueup.models;
 
+import android.util.Log;
+
+import java.util.HashMap;
+
 public class User {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String emailAddress;
+    private String phoneNumber;
+    private String role;
+    private String uuid;
+    private String profileImageUrl;
+    private HashMap<String, String> notifications;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String username, String emailAddress, String phoneNumber, String uuid) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+        Log.d("User", "Profile Image URL: " + profileImageUrl);
+    }
+
+    public HashMap<String, String> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(HashMap<String, String> notifications) {
+        this.notifications = notifications;
+    }
 }
