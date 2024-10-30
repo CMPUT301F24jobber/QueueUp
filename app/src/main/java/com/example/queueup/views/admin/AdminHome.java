786d55c8d722ecfb9f8eead9e2ed4a4cc70a2834
Kevin <khuai@ualberta.ca>
2024-10-30 17:18:15 -0600

    Fixing AdminHome

--- app/src/main/java/com/example/queueup/views/admin/AdminHome.java
+++ app/src/main/java/com/example/queueup/views/admin/AdminHome.java
@@ -1,21 +1,14 @@
 package com.example.queueup.views.admin;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.queueup.R;
 import com.example.queueup.controllers.UserController;
-import com.example.queueup.models.User;  // Ensure this model class exists in your project
-import com.example.queueup.views.admin.AdminHomeFragment;
-import com.example.queueup.views.admin.AdminUsersFragment;
-import com.example.queueup.views.admin.AdminGalleryFragment;
-import com.example.queueup.views.admin.AdminProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class AdminHome extends AppCompatActivity {
 
@@ -46,33 +39,33 @@ public class AdminHome extends AppCompatActivity {
                     .commit();
         }
 
-        // Bottom navigation setup
-        navigationView.setOnItemSelectedListener(menuItem -> {
-            switch (menuItem.getItemId()) {
-//                case R.id.navigation_home:
-//                    getSupportFragmentManager().beginTransaction()
-//                            .setReorderingAllowed(true)
-//                            .replace(R.id.admin_activity_fragment, new AdminHomeFragment())
-//                            .commit();
-//                    break;
-//                case R.id.navigation_users:
-//                    getSupportFragmentManager().beginTransaction()
-//                            .setReorderingAllowed(true)
-//                            .replace(R.id.admin_activity_fragment, new AdminUsersFragment())
-//                            .commit();
-//                    break;
-//                case R.id.navigation_gallery:
-//                    getSupportFragmentManager().beginTransaction()
-//                            .setReorderingAllowed(true)
-//                            .replace(R.id.admin_activity_fragment, new AdminGalleryFragment())
-//                            .commit();
-//                    break;
-//                case R.id.navigation_profile:
-//                    getSupportFragmentManager().beginTransaction()
-//                            .setReorderingAllowed(true)
-//                            .replace(R.id.admin_activity_fragment, new AdminProfileFragment())
-//                            .commit();
-//                    break;
+        navigationView.setOnItemSelectedListener( menuItem -> {
+            String title = String.valueOf(menuItem.getTitle());
+            switch (title) {
+                case "Home":
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .add(R.id.admin_activity_fragment, AdminHomeFragment.class, null)
+                            .commit();
+                    break;
+                case "Users":
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .add(R.id.admin_activity_fragment, AdminUsersFragment.class, null)
+                            .commit();
+                    break;
+                case "Gallery":
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .add(R.id.admin_activity_fragment, AdminGalleryFragment.class, null)
+                            .commit();
+                    break;
+                case "Profile":
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .add(R.id.admin_activity_fragment, AdminProfileFragment.class, null)
+                            .commit();
+                    break;
                 default:
                     break;
             }
@@ -80,40 +73,4 @@ public class AdminHome extends AppCompatActivity {
         });
     }
 
-    /**
-     * Fetches user data from Firestore using the deviceId and updates the UI.
-     */
-    private void fetchUserData() {
-        db.collection("users")
-                .whereEqualTo("deviceId", deviceId)
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful() && task.getResult() != null && !task.getResult().isEmpty()) {
-                        // Device ID found in Firestore
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            User user = document.toObject(User.class);
-                            String firstName = user.getFirstName();
-
-                            if (firstName != null && !firstName.isEmpty()) {
-                                titleTextView.setText("Welcome, " + firstName + "!");
-                            } else {
-                                titleTextView.setText("Welcome, Admin!");  // Couldn't retrieve first name
-                            }
-                        }
-                    } else {
-                        Log.d("AdminHome", "No user found with this device ID");
-                        titleTextView.setText("Welcome, Admin!");  // If no user found
-                    }
-                })
-                .addOnFailureListener(e -> Log.e("AdminHome", "Error fetching user data", e));
-    }
-
-    /**
-     * Called when the activity has become visible. Used here to refresh the user data.
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        fetchUserData();  // Re-fetch user data to update the UI with any changes made
-    }
 }
