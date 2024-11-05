51f839432448f3898730582e9bd654ec1f91c192
Kevin <khuai@ualberta.ca>
2024-11-05 10:46:59 -0700

    Integrated fragments into poster

--- app/src/main/java/com/example/queueup/views/attendee/AttendeeEvent.java
+++ app/src/main/java/com/example/queueup/views/attendee/AttendeeEvent.java
@@ -5,8 +5,10 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.queueup.R;
+import com.example.queueup.views.admin.AdminHomeFragment;
 
 public class AttendeeEvent extends AppCompatActivity {
     private ImageButton backButton;
@@ -15,7 +17,12 @@ public class AttendeeEvent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.attendee_poster_activity);
 
-
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.attendee_event_fragment, AttendeeWaitlistFragment.class, null)
+                    .commit();
+        }
         backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener((view) -> {
             onBackPressed();
