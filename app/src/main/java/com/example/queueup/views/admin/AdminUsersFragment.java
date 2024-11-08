2909f2f9b0d049cc310c7b9b11f9c8b286128cff
Kevin <khuai@ualberta.ca>
2024-11-08 11:37:20 -0700

    fixed user list clicking

--- app/src/main/java/com/example/queueup/views/admin/AdminUsersFragment.java
+++ app/src/main/java/com/example/queueup/views/admin/AdminUsersFragment.java
@@ -17,11 +17,6 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 
-/**
- * AdminUsersFragment is responsible for displaying the list of users in the admin section of the app.
- * It listens for real-time updates to the Firestore 'users' collection and allows the admin to view detailed information
- * about a user by clicking on their profile.
- */
 public class AdminUsersFragment extends Fragment implements AdminClickUserFragment.RefreshUsersListener {
 
     public AdminUsersFragment() {
@@ -33,14 +28,6 @@ public class AdminUsersFragment extends Fragment implements AdminClickUserFragme
     private UsersArrayAdapter usersAdapter;
     private FirebaseFirestore db;
 
-    /**
-     * Called when the fragment's view is created. This method initializes the UI elements, sets up an adapter
-     * for displaying the list of users, and sets up an item click listener to navigate to the AdminUserFragment
-     * for detailed information about the selected user.
-     *
-     * @param view The View returned by onCreateView().
-     * @param savedInstanceState A Bundle containing the activity's previous state (if any).
-     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -56,24 +43,21 @@ public class AdminUsersFragment extends Fragment implements AdminClickUserFragme
         userList.setOnItemClickListener((parent, view1, position, id) -> {
             User selectedUser = dataList.get(position);
 
-            AdminClickUserFragment fragment = new AdminClickUserFragment();
+            AdminUserFragment fragment = new AdminUserFragment();
 
             Bundle args = new Bundle();
             args.putParcelable("user", selectedUser);
 
-            fragment.setArguments(args);
-
-            fragment.show(getParentFragmentManager(), "AdminClickUserFragment");
-
+            getParentFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.admin_activity_fragment, AdminUserFragment.class, args)
+                    .addToBackStack("User")
+                    .commit();
         });
 
 
         listenToUsersCollection();
     }
-
-    /**
-     * Called when the fragment resumes, this method ensures that the fragment's user list is refreshed.
-     */
     @Override
     public void onResume() {
         super.onResume();
