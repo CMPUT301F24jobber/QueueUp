356015d0a0ada1c3926c077ca3eaf76284d8155a
Kevin <khuai@ualberta.ca>
2024-12-01 19:37:23 -0700

    Test functionality

--- app/src/main/java/com/example/queueup/views/organizer/OrganizerCreateEvent.java
+++ app/src/main/java/com/example/queueup/views/organizer/OrganizerCreateEvent.java
@@ -271,7 +271,7 @@ public class OrganizerCreateEvent extends AppCompatActivity {
         String eventId = UUID.randomUUID().toString();
         String qrCodeId = UUID.randomUUID().toString();
         ImageUploader imageUploader = new ImageUploader();
-        if (description == "test") {
+        if (description.equals("test")) {
             Event newEvent = new Event(
                     eventId,
                     eventName,
